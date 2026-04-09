f156f582cdb7137a1e8bdae20b9fe5f51117a935
Purnith <achuthavenkatasatyanarayana@gmail.com>
2026-04-09 22:24:09 +0530

    Adding Two Numbers

---
+++ Basics/adding_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b; // 5 & 6
+    cout << "The sum of " << a << " and " << b << " is " << a + b << endl; // 11
+    return 0;
+}
+
+/*
+Output:
+The sum of 5 and 6 is 11
+*/
